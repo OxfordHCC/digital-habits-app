2cba2f59103621888f1d41efad8acc2d7ca9d6b6
Konrad Kollnig <5175206+kasnder@users.noreply.github.com>
2020-03-04 12:25:30 +0000

    add notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { IonicStorageModule } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,8 @@ import { IonicStorageModule } from '@ionic/storage';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    LocalNotifications
   ],
   exports: [],
   bootstrap: [AppComponent]
--- src/app/tip/tip.component.ts
+++ src/app/tip/tip.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {DataService, Tip} from '../services/data.service';
+import { ToastController } from '@ionic/angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-tip',
@@ -9,7 +11,34 @@ import {DataService, Tip} from '../services/data.service';
 export class TipComponent implements OnInit {
   @Input() tip: Tip;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService,
+              private toastController: ToastController,
+              private localNotifications: LocalNotifications) { }
 
   ngOnInit() {}
+
+  async toggleGoal() {
+    await this.data.toggleGoal(this.tip.id);
+
+    if (this.data.findGoal(this.tip.id) >= 0) {
+      const toast = await this.toastController.create({
+        message: 'Goal added. A reminder will be shown in 7 days.',
+        duration: 2000
+      });
+      await toast.present();
+
+      const sevenDays = new Date();
+      sevenDays.setDate(sevenDays.getDate() + 7);
+
+      // Check out: https://stackoverflow.com/questions/53925894/ionic-what-is-the-correct-way-to-change-icon-in-local-notifications-plugin/54115579#54115579
+      this.localNotifications.schedule({
+        text: 'Keep track of your goals',
+        title: this.tip.title,
+        trigger: {at: sevenDays},
+        smallIcon: 'res://ic_stat_notify',
+        // icon: 'file://assets/public/assets/icon/favicon.png',
+        sound: null
+      });
+    }
+  }
 }
